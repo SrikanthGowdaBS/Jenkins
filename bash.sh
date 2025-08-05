3d5d1e4e8329ce7a831052d8ff405b82e1789bc1
Srikanth Gowda B S <96198436+SrikanthGowdaBS@users.noreply.github.com>
2025-08-05 10:58:30 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 echo "📦 Starting Java build..."
-mvn clean package
+
+mvn clean install
+
+if [ $? -eq 0 ]; then
+    echo "✅ Build successful"
+else
+    echo "❌ Build failed"
+    exit 1
+fi
